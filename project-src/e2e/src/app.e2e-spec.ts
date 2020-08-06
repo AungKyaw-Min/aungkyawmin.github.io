22b90ddab9ff4375cb0aa5d4a88141bb24a84135
Aung Kyaw Min <koaungkyawmin23@gmail.com>
2020-08-06 13:38:02 -0500

    chnage my name

--- project-src/e2e/src/app.e2e-spec.ts
+++ project-src/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-portfolio app is running!');
+    expect(page.getTitleText()).toEqual('AungKyawMin app is running!');
   });
 
   afterEach(async () => {
